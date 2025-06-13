b7c6fa2ce3a9384368b20ba0ba29dddc71479c8a
Leudy Nolasco <randylnolasco7@gmail.com>
2025-06-13 12:07:25 -0400

    optimizacion

--- BR-front/BR-back/routes/clientes.js
+++ BR-front/BR-back/routes/clientes.js
@@ -17,35 +17,39 @@ router.get("/", async (req, res) => {
     const connection = await getOracleConnection();
 
 const query = `
+
 SELECT
-    TO_CHAR(o.ou_num) AS Identificacion,
-    o.alias_name AS NombreCompleto,
-    x.x_attrib_110 AS NombreComercial,
-    f.desc_text AS TipoCliente,
-    x.x_attrib_85 AS TipoPersona,
-    TO_CHAR(o.cust_stat_cd) AS Estado,
-    h.x_codigo AS SegmentoCliente,
-    'NO DISPONIBLE' AS Riesgo, -- 🔹 No hay una columna específica para "Riesgo", así que usamos un valor por defecto
-    'NO DISPONIBLE' AS FechaVencimiento -- 🔹 Usamos un valor por defecto si la columna no existe
-FROM siebel.s_org_ext o
-LEFT JOIN siebel.s_org_ext_x x ON o.row_id = x.par_row_id
-LEFT JOIN siebel.eai_view_lst_of_val f ON f.val = x.x_attrib_105 AND f.type = 'BR_PJ_TIPO_CLIENTE' AND f.lang_id = 'ESN' AND f.active_flg = 'Y'
-LEFT JOIN siebel.cx_segmentos h ON h.row_id = o.x_seg_id
+  CAST(NULL AS NUMBER) AS id,
+  TRIM(A.FST_NAME || ' ' || A.MID_NAME || ' ' || A.LAST_NAME || ' ' || A.MAIDEN_NAME) AS nombre,
+  A.SOC_SECURITY_NUM AS cedula,
+  'activo' AS estado,
+  H.X_CODIGO AS segmento,
+  'Bajo' AS categoria,
+  'Física' AS tipoPersona,
+  TO_CHAR(X.X_FECHA_VENC_PASA, 'YYYY-MM-DD') AS fechaVenc
+FROM SIEBEL.S_CONTACT A
+LEFT JOIN SIEBEL.CX_SEGMENTOS H ON H.ROW_ID = A.X_SEG_ID
+LEFT JOIN SIEBEL.S_CONTACT_X X ON X.PAR_ROW_ID = A.ROW_ID
 
 UNION ALL
 
 SELECT
-    TO_CHAR(A.SOC_SECURITY_NUM) AS Identificacion,
-    A.FST_NAME || ' ' || NVL(A.MID_NAME, '') || ' ' || A.LAST_NAME || ' ' || NVL(A.MAIDEN_NAME, '') AS NombreCompleto,
-    'SIN NOMBRE COMERCIAL' AS NombreComercial,
-    'SIN TIPO CLIENTE' AS TipoCliente,
-    'Persona Física' AS TipoPersona,
-    TO_CHAR(A.CUST_STAT_CD) AS Estado,
-    NVL(H.X_CODIGO, 'SIN SEGMENTO') AS SegmentoCliente,
-    'NO DISPONIBLE' AS Riesgo, -- 🔹 No hay una columna específica para "Riesgo", así que usamos un valor por defecto
-    'NO DISPONIBLE' AS FechaVencimiento -- 🔹 Usamos un valor por defecto si la columna no existe
-FROM SIEBEL.S_CONTACT A
-LEFT JOIN SIEBEL.CX_SEGMENTOS H ON H.ROW_ID = A.X_SEG_ID;
+  CAST(NULL AS NUMBER) AS id,
+  o.alias_name AS nombre,
+  o.ou_num AS cedula,
+  o.cust_stat_cd AS estado,
+  h.x_codigo AS segmento,
+  f.desc_text AS categoria,
+  x.x_attrib_85 AS tipoPersona,
+  NULL AS fechaVenc
+FROM siebel.s_org_ext o
+LEFT JOIN siebel.s_org_ext_x x ON o.row_id = x.par_row_id
+LEFT JOIN siebel.eai_view_lst_of_val f
+  ON f.val = x.x_attrib_105
+  AND f.type = 'BR_PJ_TIPO_CLIENTE'
+  AND f.lang_id = 'ESN'
+  AND f.active_flg = 'Y'
+LEFT JOIN siebel.cx_segmentos h ON h.row_id = o.x_seg_id
 
     `;
 
